c6fb8863118623aadf9f315d1f6a48755dd9468e
Richard Walsh <richwalsh2@googlemail.com>
2021-06-21 17:21:22 +0100

    remove console.log. tidy js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// *************************************************************************************************
-// CHANGED DIVS CONTAINERS FROM BTNS FOR CSS STYLE. NEED TO CHANGE NODES ON ID SELECTORS TO GET THEM TO WORK. SEE HTML
-// CHANGED HTML. NEED TO CHANGE HTML FOR RENDER FUNCTION. ADDED LINE DIV AND REMOVED SOME ON BUTTONS
-// **************************************************************************************************
-
-let log = console.log;
-
 let myLibrary = [];
-
-let bookArray = 0;
 
 function book(id, title, author, pages, status) {
   this.id = id;
@@ -21,9 +12,6 @@ function book(id, title, author, pages, status) {
     });
 }
 
-// const newBook1 = new book('The Bible', 'Jesus', '510', 'it has not been read');
-// console.log(newBook1.report());
-
 let addBookToLibrary = function () {
   let id, title, author, pages, status;
 
@@ -32,8 +20,6 @@ let addBookToLibrary = function () {
   } else {
     id = 0;
   }
-
-  console.log(id);
 
   title = document.getElementById('b-title').value;
   author = document.getElementById('b-author').value;
@@ -48,12 +34,11 @@ let addBookToLibrary = function () {
   let newBook = new book(id, title, author, pages, status);
   myLibrary.push(newBook);
   document.forms[0].reset();
-  log(myLibrary);
+
   render();
 };
 
 let render = function () {
-  //  let html = '<div class="book" id="bookid-%id%"> <div> <button class="btn btn__delete" id="btn__delete">delete</button> </div> <h2 class="book__header">BOOK TITLE: <span id="book-info__title-%num%"></span></h2> <h3 class="book__author">BOOK AUTHOR: <span id="book-info__author-%num%"></span></h3> <h3 class="book__pages">NUMBER OF PAGES: <span id="book-info__pages-%num%"></span></h3> <h3 class="book__read">BOOK HAS BEEN READ?: <span id="book-info__read-%num%"></span></h3> <div> <button class="btn btn__read" id="btn__read-yes">read yes</button> </div> <div> <button class="btn btn__read" id="btn__read-no">read no</button> </div> </div>'
   let html = `<div class="book" id="bookid-%id%"> <div> <button class="btn btn__delete" id="btn__delete">delete</button> </div> <div class="line line-long"></div> <h3 class="book__header">BOOK TITLE: <span id="book-info__title-%num%">FONT TEST</span></h3> <div class="line"></div> <h4 class="book__author">BOOK AUTHOR: <span id="book-info__author-%num%">FONT TEST</span></h4> <div class="line"></div> <h4 class="book__pages">NUMBER OF PAGES: <span id="book-info__pages-%num%">FONT TEST</span></h4> <div class="line"></div> <h4 class="book__read">STATUS: <span id="book-info__read-%num%"></span></h4> <div class="line line-long"></div> <div> <button class="btn btn__read" id="btn__read-yes">Avaliable</button>   <button class="btn btn__read" id="btn__read-no">Unavaliable</button> </div> </div>`;
   const parent = document.querySelector('.book__card');
 
@@ -88,7 +73,6 @@ let render = function () {
       'Unavaliable'
     ) {
       document.getElementById(`bookid-${book.id}`).classList.add('book-NA');
-      log(`book`);
     } else if (
       document.getElementById(`book-info__read-${i}`).textContent == 'Avaliable'
     ) {
@@ -101,18 +85,8 @@ let render = function () {
 
 let deleteBook = function (event) {
   let bookNodeID, dBook, libraryID, libraryIndex, bookID, splitBookID;
-  // console.log('work1');
+
   bookNodeID = event.target.parentNode.parentNode.id;
-  log(event.target.id);
-  // console.log('work2');
-  // console.log(bookID);
-  // console.log('work3');
-
-  // console.log(dBook.parentNode)
-
-  // console.log('work4');
-
-  // console.log(event.target.parentNode.parentNode.id);
 
   if (bookNodeID && event.target.id == 'btn__delete') {
     splitBookID = bookNodeID.split('-');
@@ -125,17 +99,12 @@ let deleteBook = function (event) {
     return current.id;
   });
 
-  log(libraryID);
-
   libraryIndex = libraryID.indexOf(bookID);
-
-  log(libraryIndex);
 
   if (libraryIndex !== -1) {
     myLibrary.splice(libraryIndex, 1);
   }
 
-  log(myLibrary);
   let numBook = myLibrary.length;
   numberOfBook();
   render();
@@ -170,9 +139,6 @@ let changeStatus = function (event) {
 
   bookNodeID = event.target.parentNode.parentNode.id;
 
-  log(bookNodeID);
-  log('this is booknodeid for status');
-
   if (
     (bookNodeID && event.target.id == 'btn__read-yes') ||
     (bookNodeID && event.target.id == 'btn__read-no')
@@ -185,15 +151,9 @@ let changeStatus = function (event) {
     return current.id;
   });
 
-  log(libraryID + ' this is library id');
-  log(bookID + ' this is book id');
-
   libraryIndex = libraryID.indexOf(bookID);
-  log(libraryIndex + ' this is library index');
 
   bookNodeYesNo = event.target.id;
-  log(event.target.id);
-  log(bookNodeYesNo + 'yesno');
 
   if (bookNodeYesNo == 'btn__read-yes') {
     document.getElementById(`book-info__read-${libraryIndex}`).textContent =
@@ -217,43 +177,11 @@ let changeStatus = function (event) {
   }
 };
 
-const testpress = () => {
-  console.log('btn-add press');
-};
-
-const testclose = () => {
-  console.log('btn-close press');
-};
-
 document.getElementById('btn__form').addEventListener('click', openForm);
 document.getElementById('btn__close').addEventListener('click', closeForm);
-document.getElementById('btn__close').addEventListener('click', testclose);
-document.getElementById('btn__add').addEventListener('click', testpress);
+
 document.getElementById('btn__add').addEventListener('click', addBookToLibrary);
 document.querySelector('.container').addEventListener('click', deleteBook);
 document.querySelector('.container').addEventListener('click', changeStatus);
 
-// const testString = ('id-55565456564');
-// let testSplit = testString.split("-")
-// console.log(testSplit);
 numberOfBook();
-
-// NOTES **************************************************************************************************************
-
-// let render = function () {
-//   for (i = 0; i < myLibrary.length; i++) {
-//     let book = myLibrary[i]
-//     log(myLibrary[i])
-//     document.querySelector(`#book-info__${i+1}`).innerHTML = `${book.title}`
-//     document.querySelector(`#book-info__${i+2}`).innerHTML = `${book.author}`
-//     document.querySelector(`#book-info__${i+3}`).innerHTML = `${book.pages}`
-//     document.querySelector(`#book-info__${i+4}`).innerHTML = `${book.haveRead}`
-
-//     // document.querySelector('#main').innerHTML = book.author
-//   }
-// }
-
-// addBookToLibrary('bible', 'jesus','500','no');
-// addBookToLibrary('got', 'test6','test7','test8');
-// addBookToLibrary('lotr', 'test10','test11','test12');
-// addBookToLibrary('got', 'test6','test7','test8');
